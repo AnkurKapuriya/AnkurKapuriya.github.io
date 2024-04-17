75183ce7bf388e0cf92bc2a4bc8b5731e4d0f117
AnkurKapuriya <167308882+AnkurKapuriya@users.noreply.github.com>
2024-04-17 18:31:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function hello(){
+function clickTimeCard(){
 document.getElementsByClassName("x1gd")[1].click();
+}
+
+function setValues(value){
+
+  var strValue = String(value);
+
+  document.querySelector('input[aria-label=" Person Number"]').value=strValue;
+  
 }
