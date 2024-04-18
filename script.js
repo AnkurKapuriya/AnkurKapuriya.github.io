e3448c5b6a0323f80df1d5a5b3a1e3d9c4dcd542
AnkurKapuriya <167308882+AnkurKapuriya@users.noreply.github.com>
2024-04-18 14:49:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ function setValues(value,FromDate,ToDate){
   document.querySelectorAll('input[aria-label="dd-mmm-yyyy"]')[0].value=strValurFromDate;
 
   document.querySelectorAll('input[aria-label="dd-mmm-yyyy"]')[1].value=strValurToDate;
-
-
-  
   
 }
+
+function ClickCreateButton(){
+  document.querySelector('div[title="Create"]').click();
+}
+
