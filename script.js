2d67c0d52b22840b2823e0c94826d869d5dde092
AnkurKapuriya <167308882+AnkurKapuriya@users.noreply.github.com>
2024-04-18 09:41:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ function clickTimeCard(){
 document.getElementsByClassName("x1gd")[1].click();
 }
 
-function setValues(value,FromDate){
+function setValues(value,FromDate,ToDate){
 
   var strValueNumber = String(value);
 
   var strValurFromDate= String(FromDate);
 
-  document.querySelector('input[aria-label=" Person Number"]').value=strValue;
+  var strValurToDate= String(ToDate);
+  
+  document.querySelector('input[aria-label=" Person Number"]').value=strValueNumber;
 
-  document.querySelector('input[aria-label="dd-mmm-yyyy"]').value=strValurFromDate;
+  document.querySelector('input[aria-label="dd-mmm-yyyy"]')[0].value=strValurFromDate;
+
+  document.querySelector('input[aria-label="dd-mmm-yyyy"]')[1].value=strValurToDate;
+
+
+  
   
 }
